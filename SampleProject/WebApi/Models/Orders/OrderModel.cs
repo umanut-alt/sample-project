4e9becad00e3f98b4a1c779621dcb0fb6b861fb7
umanut-alt <uma.nuthulaganti@gmail.com>
2025-07-14 13:59:01 -0400

    Add files via upload

---
+++ SampleProject/WebApi/Models/Orders/OrderModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApi.Models.Orders
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<int> ProductIds { get; set; }
+    }
+}
---
+++ SampleProject/WebApi/Models/Users/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using BusinessEntities;
+
+namespace WebApi.Models.Users
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public UserTypes Type { get; set; }
+        public int age { get; set; }
+        public decimal? AnnualSalary { get; set; }
+        public IEnumerable<string> Tags { get; set; }
+    }
+}
